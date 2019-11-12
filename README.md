f6e0d82b57a6acdb7e816c24b1f857c5aa999469
Raghvendra Kumar <mail.raghvendra.kumar@gmail.com>
2019-11-12 20:01:33 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### Gradle boilerplate
+
+- [Run a Java main Method Using Gradle]()
+
+   `./gradlew run`
+
+
---
+++ src/main/java/com/raghav/gradle/MainClass.java
@@ -0,0 +1,8 @@
+package com.raghav.gradle;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hi! from main method...");
+    }
+}
